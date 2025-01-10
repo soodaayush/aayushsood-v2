fcb767607fed065bd2a9fe911d3b48fc99dd3654
soodaayush <aayush.sood@icloud.com>
2025-01-10 17:23:01 -0400

    Added blog post page functionality

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,12 +1,13 @@
 "use client";
 
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 import blogData from "../../data/blogs.json";
 import Image from "next/image";
 
+import styles from "../../styles/blog/blogPost.module.css";
+
 export default function BlogPostPage() {
-  const router = useRouter();
-  const { id } = router.query;
+  const id = usePathname().slice(6);
   const post = blogData.find((post) => post.id === id);
 
   if (!post) {
@@ -14,50 +15,52 @@ export default function BlogPostPage() {
   }
 
   return (
-    <div className={styles.container}>
-      <h1>{post.title}</h1>
-      <p>{post.date}</p>
-      <div>
-        {post.content.map((block, index) => {
-          switch (block.type) {
-            case "paragraph":
-              return <p key={index}>{block.text}</p>;
-            case "image":
-              return (
-                <div key={index}>
-                  <Image
-                    src={block.src}
-                    alt={block.alt}
-                    width={800}
-                    height={400}
-                    className={styles.image}
-                  />
-                </div>
-              );
-            case "code":
-              return (
-                <pre key={index} className={styles.code}>
-                  <code>{block.content}</code>
-                </pre>
-              );
-            case "video":
-              return (
-                <div key={index}>
-                  <iframe
-                    width="800"
-                    height="450"
-                    src={block.src}
-                    title={block.title}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                  ></iframe>
-                </div>
-              );
-            default:
-              return null;
-          }
-        })}
+    <div className={styles.blogPostContainer}>
+      <div className={`content ${styles.blogPostContent}`}>
+        <h1>{post.title}</h1>
+        <p>{post.date}</p>
+        <div>
+          {post.content.map((block, index) => {
+            switch (block.type) {
+              case "paragraph":
+                return <p key={index}>{block.text}</p>;
+              case "image":
+                return (
+                  <div key={index}>
+                    <Image
+                      src={block.src}
+                      alt={block.alt}
+                      width={800}
+                      height={400}
+                      className={styles.image}
+                    />
+                  </div>
+                );
+              case "code":
+                return (
+                  <pre key={index} className={styles.code}>
+                    <code>{block.content}</code>
+                  </pre>
+                );
+              case "video":
+                return (
+                  <div key={index} className={styles.videoContainer}>
+                    <iframe
+                      width="800"
+                      height="450"
+                      src={block.src}
+                      title={block.title}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                );
+              default:
+                return null;
+            }
+          })}
+        </div>
       </div>
     </div>
   );
