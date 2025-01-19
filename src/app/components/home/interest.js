f3a2fe801d66e08d13d030fad58ac35dd047d4b8
soodaayush <aayush.sood@icloud.com>
2025-01-19 18:18:14 -0400

    Changed hover effect of interests

--- src/app/components/home/interest.js
+++ src/app/components/home/interest.js
@@ -9,11 +9,10 @@ export default function Interest(props) {
     <motion.div
       initial={{ scale: 1, opacity: 0.9 }}
       whileHover={{
-        scale: 1.1,
-        opacity: 1,
-        rotate: 5,
-        boxShadow: "0px 4px 20px rgba(255, 255, 255, 0.3)",
-        transition: { type: "spring", stiffness: 300, damping: 15 },
+        scale: 1.1, 
+        boxShadow: "0px 10px 15px rgba(0, 0, 0, 0.2)", 
+        transition: { duration: 0.3 }, 
+        backgroundColor: "#2f4f4f", 
       }}
       className={styles.interestContainer}
     >
@@ -23,19 +22,21 @@ export default function Interest(props) {
       </h1>
       <p>{props.description}</p>
       <div className={styles.links}>
-        <p>My favorite channels I follow are: </p>
-        {props.channels
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map((channel, index) => (
-            <a
-              className={styles.link}
-              key={index}
-              href={channel.link}
-              target="_blank"
-            >
-              {channel.name}
-            </a>
-          ))}
+        <p>My favorite creators I follow are: </p>
+        <div className={styles.linkTexts}>
+          {props.channels
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map((channel, index) => (
+              <a
+                className={styles.link}
+                key={index}
+                href={channel.link}
+                target="_blank"
+              >
+                {channel.name}
+              </a>
+            ))}
+        </div>
       </div>
     </motion.div>
   );
