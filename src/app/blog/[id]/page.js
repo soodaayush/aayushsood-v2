1a2b7fd75c6ab794ccc1c32532435e06ee6ef3b0
soodaayush <aayush.sood@icloud.com>
2025-01-19 21:11:34 -0400

    Removed blog temporarily

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,41 +1,41 @@
-import { promises as fs } from "fs";
-import path from "path";
-import { MDXRemote } from "next-mdx-remote/rsc";
-import matter from "gray-matter";
-import styles from "../../styles/blog/blogPost.module.css";
-import { MDXComponents } from "../../MDXComponents/MDXComponents";
+// import { promises as fs } from "fs";
+// import path from "path";
+// import { MDXRemote } from "next-mdx-remote/rsc";
+// import matter from "gray-matter";
+// import styles from "../../styles/blog/blogPost.module.css";
+// import { MDXComponents } from "../../MDXComponents/MDXComponents";
 
-export default async function BlogPost({ params }) {
-  const { id } = await params;
+// export default async function BlogPost({ params }) {
+//   const { id } = await params;
 
-  try {
-    const filePath = path.join(process.cwd(), "src/app/posts", `${id}.mdx`);
-    const source = await fs.readFile(filePath, "utf-8");
+//   try {
+//     const filePath = path.join(process.cwd(), "src/app/posts", `${id}.mdx`);
+//     const source = await fs.readFile(filePath, "utf-8");
 
-    const { content, data: frontmatter } = matter(source);
+//     const { content, data: frontmatter } = matter(source);
 
-    const components = {};
+//     const components = {};
 
-    return (
-      <div className={styles.blogPostContainer}>
-        <div className={`content ${styles.blogPostContent}`}>
-          <h1>{frontmatter.title}</h1>
-          <p>{frontmatter.date}</p>
-          <MDXRemote
-            source={content}
-            components={MDXComponents}
-            options={{
-              parseFrontmatter: true,
-              mdxOptions: {
-                development: process.env.NODE_ENV === "development",
-              },
-            }}
-          />
-        </div>
-      </div>
-    );
-  } catch (error) {
-    console.error("Error reading MDX file:", error);
-    return <p>Post not found. Please check the console for errors.</p>;
-  }
-}
+//     return (
+//       <div className={styles.blogPostContainer}>
+//         <div className={`content ${styles.blogPostContent}`}>
+//           <h1>{frontmatter.title}</h1>
+//           <p>{frontmatter.date}</p>
+//           <MDXRemote
+//             source={content}
+//             components={MDXComponents}
+//             options={{
+//               parseFrontmatter: true,
+//               mdxOptions: {
+//                 development: process.env.NODE_ENV === "development",
+//               },
+//             }}
+//           />
+//         </div>
+//       </div>
+//     );
+//   } catch (error) {
+//     console.error("Error reading MDX file:", error);
+//     return <p>Post not found. Please check the console for errors.</p>;
+//   }
+// }
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,54 +1,54 @@
-import styles from "../styles/blog/blog.module.css";
-import Image from "next/image";
+// import styles from "../styles/blog/blog.module.css";
+// import Image from "next/image";
 
-import Link from "next/link";
+// import Link from "next/link";
 
-import { promises as fs } from "fs";
-import path from "path";
-import { compileMDX } from "next-mdx-remote/rsc";
+// import { promises as fs } from "fs";
+// import path from "path";
+// import { compileMDX } from "next-mdx-remote/rsc";
 
-export default async function Blog() {
-  const filenames = await fs.readdir(path.join(process.cwd(), "src/app/posts"));
+// export default async function Blog() {
+//   const filenames = await fs.readdir(path.join(process.cwd(), "src/app/posts"));
 
-  const blogPosts = await Promise.all(
-    filenames.map(async (filename) => {
-      const content = await fs.readFile(
-        path.join(process.cwd(), "src/app/posts", filename),
-        "utf-8"
-      );
+//   const blogPosts = await Promise.all(
+//     filenames.map(async (filename) => {
+//       const content = await fs.readFile(
+//         path.join(process.cwd(), "src/app/posts", filename),
+//         "utf-8"
+//       );
 
-      const { frontmatter } = await compileMDX({
-        source: content,
-        options: {
-          parseFrontmatter: true,
-        },
-      });
+//       const { frontmatter } = await compileMDX({
+//         source: content,
+//         options: {
+//           parseFrontmatter: true,
+//         },
+//       });
 
-      return {
-        filename,
-        slug: filename.replace(".mdx", ""),
-        ...frontmatter,
-      };
-    })
-  );
+//       return {
+//         filename,
+//         slug: filename.replace(".mdx", ""),
+//         ...frontmatter,
+//       };
+//     })
+//   );
 
-  return (
-    <div className={styles.blogContainer}>
-      <div className={`content ${styles.blogContent}`}>
-        <h1 className={styles.blogTitle}>Blog</h1>
-        <div className={styles.blogList}>
-          {blogPosts.map((post, index) => (
-            <Link
-              className={styles.blogPost}
-              key={index}
-              href={`/blog/${post.slug}`}
-            >
-              <h1 className={styles.title}>{post.title}</h1>
-              <p className={styles.date}>{post.date}</p>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className={styles.blogContainer}>
+//       <div className={`content ${styles.blogContent}`}>
+//         <h1 className={styles.blogTitle}>Blog</h1>
+//         <div className={styles.blogList}>
+//           {blogPosts.map((post, index) => (
+//             <Link
+//               className={styles.blogPost}
+//               key={index}
+//               href={`/blog/${post.slug}`}
+//             >
+//               <h1 className={styles.title}>{post.title}</h1>
+//               <p className={styles.date}>{post.date}</p>
+//             </Link>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
