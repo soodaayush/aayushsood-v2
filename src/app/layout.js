ff1c6eaf2d225f0be34e5be68ef107a2edc2ebe8
soodaayush <aayush.sood@icloud.com>
2025-01-24 08:04:44 -0400

    Added more metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,15 @@
 import "./styles/global/globals.css";
 
+import { Viewport, Metadata } from "next";
+
 import Header from "./components/global/header";
 import Footer from "./components/global/footer";
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  themeColor: "#ffffff",
+};
 
 export const metadata = {
   title: "Aayush Sood",
@@ -14,6 +22,14 @@ export const metadata = {
     "programming",
     "projects",
   ],
+  robots: {
+    index: true,
+    follow: true,
+    "max-image-preview": "large",
+    "max-snippet": -1,
+    "max-video-preview": -1,
+    googleBot: "index, follow",
+  },
   category: "portfolio",
   openGraph: {
     title: "Aayush Sood",
@@ -25,15 +41,57 @@ export const metadata = {
         url: "https://www.aayushsood.com/assets/openGraph/banner.png",
         width: 800,
         height: 600,
+        type: "image/png",
+        alt: "Blue text on dark background",
       },
     ],
     locale: "en_US",
     type: "website",
   },
+  appleWebApp: {
+    title: "Aayush Sood",
+    statusBarStyle: "default",
+    capable: true,
+  },
   icons: {
-    icon: "/favicon/android-chrome-512x512.png",
-    shortcut: "/favicon/android-chrome-192x192.png",
-    apple: "/favicon/apple-touch-icon.png",
+    icon: [
+      {
+        url: "/favicon/favicon.ico",
+        type: "image/x-icon",
+      },
+      {
+        url: "/favicon/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/favicon/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/favicon/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    shortcut: [
+      {
+        url: "/favicon/favicon.ico",
+        type: "image/x-icon",
+      },
+    ],
+    apple: [
+      {
+        url: "/favicon/apple-touch-icon.png",
+        type: "image/png",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
