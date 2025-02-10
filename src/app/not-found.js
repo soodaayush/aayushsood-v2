f19b21c8f5026d707270bd70588290ded396d029
soodaayush <aayush.sood@icloud.com>
2025-02-10 07:59:42 -0400

    Added animation to 404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -12,7 +12,6 @@ export default function NotFound() {
     <div className={styles.notFoundContainer}>
       <div className={`content ${styles.notFoundContent}`}>
         <motion.div
-          className={`${styles.projectCard}`}
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 0.9 }}
