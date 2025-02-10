f3d757d3f7cab1a81adbf14a5d5933a802574442
soodaayush <aayush.sood@icloud.com>
2025-02-10 07:59:28 -0400

    Added animation to 404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,18 +1,23 @@
+"use client";
+
+import { motion } from "motion/react";
 import Link from "next/link";
 
 import styles from "./styles/global/404.module.css";
 
 import { GoArrowUpLeft } from "react-icons/go";
 
-export const metadata = {
-  title: "Not Found | Aayush Sood",
-};
-
 export default function NotFound() {
   return (
     <div className={styles.notFoundContainer}>
       <div className={`content ${styles.notFoundContent}`}>
-        <div>
+        <motion.div
+          className={`${styles.projectCard}`}
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.9 }}
+          transition={{ duration: 0.5 }}
+        >
           <h1 className={styles.headerText}>Not Found</h1>
           <p className={styles.labelText}>
             Sorry, there is nothing at this URL.
@@ -21,7 +26,7 @@ export default function NotFound() {
             <GoArrowUpLeft />
             Go Home
           </Link>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
