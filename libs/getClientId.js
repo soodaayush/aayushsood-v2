bfe0a2b0ff92d72ee22cda83560a42cfa81d6ec9
soodaayush <aayush.sood@icloud.com>
2026-01-03 14:40:16 -0400

    Cleaned up code

--- libs/getClientId.js
+++
@@ -1,9 +0,0 @@
-export function getClientId() {
-  if (typeof window === "undefined") return null; // SSR safe
-  let id = localStorage.getItem("clientId");
-  if (!id) {
-    id = crypto.randomUUID();
-    localStorage.setItem("clientId", id);
-  }
-  return id;
-}
