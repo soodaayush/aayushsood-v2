b15153ab292de8b98062161275f2bc4c0fcbde36
soodaayush <aayush.sood@icloud.com>
2026-04-18 19:39:02 -0300

    Reduced react-icons size in deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 const nextConfig = {
   serverExternalPackages: ["@resvg/resvg-js", "satori"],
+  experimental: {
+    optimizePackageImports: ["react-icons"],
+  },
 };
 
 export default nextConfig;
